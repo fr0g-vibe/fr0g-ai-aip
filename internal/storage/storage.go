497e3c45b35a4fb44b6ce259e999e3353dd54b4a
Your Name <you@example.com>
2025-06-29 23:40:46 -0600

    remove storage.go

--- internal/storage/storage.go
+++
@@ -1,4 +0,0 @@
-package storage
-
-// This file was created in error - the Storage interface is already defined in interface.go
-// Removing duplicate interface definition to fix redeclaration error
